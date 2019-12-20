781c6be2357367df6d9524a68f9803765625401f
Bilguun <bbg.1015@gmail.com>
2019-12-20 16:49:56 -0600

    MVP done but still working on names

--- names/names.py
+++ names/names.py
@@ -1,4 +1,7 @@
 import time
+import sys
+sys.path.append('../ring_buffer')
+from doubly_linked_list import DoublyLinkedList
 
 start_time = time.time()
 
@@ -10,11 +13,12 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -23,4 +27,4 @@ print (f"runtime: {end_time - start_time} seconds")
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish with no restrictions on techniques or data
-# structures?
+# structures?3
