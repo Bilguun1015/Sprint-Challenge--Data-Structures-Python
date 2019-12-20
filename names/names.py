7ebc52274dcad3c073d1f0435cfac592ca947cce
Bilguun <bbg.1015@gmail.com>
2019-12-20 17:00:07 -0600

    names is working

--- names/names.py
+++ names/names.py
@@ -13,8 +13,58 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-# duplicates = []
+    def insert(self, value):
+        if self.value:
+            if value < self.value:
+                if self.left:
+                    return self.left.insert(value)
+                else:
+                    self.left = BinarySearchTree(value)
+                    return self.left.value
+            elif value >= self.value:
+                if self.right:
+                    return self.right.insert(value)
+                else:
+                    self.right = BinarySearchTree(value)
+                    return self.right.value
+        else:
+            self.value = value
+            return self.value
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target >= self.value:
+            if self.right:
+                if self.right.value == target:
+                    return True
+                else:
+                    return self.right.contains(target)
+            else:
+                return False
+        elif target < self.value:
+            if self.left:
+                if self.left.value == target:
+                    return True
+                else:
+                    return self.left.contains(target)
+            else:
+                return False
+binTree = BinarySearchTree('1')
+duplicates = []
+for name in names_1:
+    binTree.insert(name)
+for name in names_2:
+    if binTree.contains(name):
+        duplicates.append(name)
+
+# 
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
